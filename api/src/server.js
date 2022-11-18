eb20c9c71630705fd8c2ca86a3515654fca246c2
Dione Santos <dionedev@gmail.com>
2022-11-17 22:06:48 -0300

    feat: first commit uploading project to github

---
+++ api/src/server.js
@@ -0,0 +1,31 @@
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+const PORT =  3333
+const routes = require('./routes')
+const AppError = require('./utils/AppError')
+
+app.use(express.json())
+
+app.use(routes)
+
+app.use((error, request, response, next ) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json(
+      {
+        status: "error",
+        message: error.message
+      }
+    )
+  }
+
+  return response.status(500).json(
+    {
+      status: "error",
+      message: "Internal server error"
+    }
+  )
+})
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
