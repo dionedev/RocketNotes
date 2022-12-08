a73ceb8e9c79d3a85586b3237d2af82f3e16f404
Dione Santos <dionedev@gmail.com>
2022-12-08 20:45:09 -0300

    feat: created migrations for the database

--- api/src/server.js
+++ api/src/server.js
@@ -1,17 +1,17 @@
 require('express-async-errors')
 
 const express = require('express')
-const database = require('./database/sqlite')
+const migrationsRun = require('./database/sqlite/migrations')
 const app = express()
 const PORT =  3333
 const routes = require('./routes')
 const AppError = require('./utils/AppError')
 
+migrationsRun()
+
 app.use(express.json())
 
 app.use(routes)
-
-database()
 
 app.use((error, request, response, next ) => {
   if(error instanceof AppError) {
